606f3da640ec72386f5267b2ff75cc6260b5c2a4
Sabrina Markon <phpsitescripts@outlook.com>
2018-06-30 13:57:04 -0600

    Add autoscroll so the message list goes to the end of the messages if a new one is added, unless user is reading messages farther up

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Message from './Message';
 
 class MessageList extends React.Component {
+
+    /* triggered right BEFORE this component will update */
+    componentWillUpdate() {
+
+        /* If the user is reading messages higher up the list of messages, and another user posts a message, it scrolls everyone reading to the bottom of the list (interrupting their reading), because of componentDidUpdate code. This will fix that: */
+        const node = ReactDOM.findDOMNode(this);
+        /* where the user is on the scroll position: 
+        clientHeight is the visible area (box)
+        The extra 100 added on is if we are within 100px of the bottom already so it doesn't have to be EXACTLY at the bottom to drop down.
+        */
+        this.shouldScrollToBottom = node.scrolltop + node.clientHeight + 100 >= node.scrollHeight
+
+    }
+
+    /* triggered right AFTER this component has updated. shouldScrollToBottom is from componentWillUpdate lifecycle method. */
+    componentDidUpdate() {
+        if (this.shouldScrollToBottom) {
+            const node = ReactDOM.findDOMNode(this);
+            /* scrollTop is how far we've scrolled down, and scrollHeight is how long our entire scrollable element is. We want the chat to automatically scroll to the end of the messages so we can see the last one */
+            node.scrollTop = node.scrollHeight;
+        }
+    }
 
     render() {
         return(
